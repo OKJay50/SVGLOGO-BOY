24957bfa8486de1881c1a153158031083e3999ff
Joseph DeMasse <81646579+OKJay50@users.noreply.github.com>
2023-11-15 15:56:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const { Square, Circle, Triangle } = require('./lib/shapes');
 
 class Svg {
     constructor(textChoice, shapeChoice) {
@@ -12,7 +11,7 @@ class Svg {
     }
 
     setText(text, textColor) {
-        this.textChoice = `<text x="150" y="125" font-size="75" text-anchor="center" fill="${textColor}">${text}</text>`;
+        this.textChoice = `<text x="150" y="125" font-size="20" text-anchor="middle" fill="${textColor}">${text}</text>`;
     }
 
     setShape(shape) {
@@ -30,60 +29,63 @@ function writeToFile(fileName, data) {
 }
 
 async function promptUser() {
-    const inquirerModule = await import('inquirer');
-    const inquirer = inquirerModule.default;
+    try {
+        const inquirerModule = await import('inquirer');
+        const inquirer = inquirerModule.default;
 
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What text would you like in your image? (Please make sure it is 3 characters or less)',
-            name: 'text',
-            validate: (input) => input.length <= 3,
-        },
-        {
-            type: 'input',
-            message: 'What color would you like your text to be? (You may use a keyword or hexadecimal)',
-            name: 'textcolor',
-        },
-        {
-            type: 'list',
-            message: 'What shape would you like to use?',
-            name: 'shape',
-            choices: ['Square', 'Circle', 'Triangle'],
-        },
-        {
-            type: 'input',
-            message: 'What color would you like your shape to be? (You may use a keyword or hexadecimal)',
-            name: 'shapecolor',
-        },
-    ])
-    .then((responses) => {
-        let chosenText = responses.text;
-        let chosenTextColor = responses.textcolor;
-        let chosenShapeColor = responses.shapecolor;
-        let chosenShape = responses.shape;
+        const responses = await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What text would you like in your image? (Please make sure it is 3 characters or less)',
+                name: 'text',
+                validate: (input) => input.length <= 3,
+            },
+            {
+                type: 'input',
+                message: 'What color would you like your text to be? (You may use a keyword or hexadecimal)',
+                name: 'textcolor',
+            },
+            {
+                type: 'list',
+                message: 'What shape would you like to use?',
+                name: 'shape',
+                choices: ['Rectangle', 'Ellipse', 'Pentagon'],
+            },
+            {
+                type: 'input',
+                message: 'What color would you like your shape to be? (You may use a keyword or hexadecimal)',
+                name: 'shapecolor',
+            },
+        ]);
+
+        const { Rectangle, Ellipse, Pentagon } = await import('./lib/shapes.js');
         let shapeElement;
 
-        if (chosenShape === "Square") {
-            shapeElement = new Square();
-        } else if (chosenShape === "Circle") {
-            shapeElement = new Circle();
-        } else if (chosenShape === "Triangle") {
-            shapeElement = new Triangle();
+        switch (responses.shape) {
+            case "Rectangle":
+                shapeElement = new Rectangle();
+                break;
+            case "Ellipse":
+                shapeElement = new Ellipse();
+                break;
+            case "Pentagon":
+                shapeElement = new Pentagon();
+                break;
+            default:
+                throw new Error('Invalid shape choice');
         }
 
-        shapeElement.chooseColor(chosenShapeColor);
+        shapeElement.setFillColor(responses.shapecolor);
 
         let svg = new Svg('', '');
-
-        svg.setText(chosenText, chosenTextColor);
+        svg.setText(responses.text, responses.textcolor);
         svg.setShape(shapeElement);
 
         let svgContent = svg.render();
-        let svgFileName = "logo.svg";
-
-        writeToFile(svgFileName, svgContent);
-    });
+        writeToFile("logo.svg", svgContent);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 promptUser();
